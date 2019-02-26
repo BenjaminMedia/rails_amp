77abffb11351daaa4fec1625f9c79108a7348349
Yiannis Van Halewijck <yiannisvh@benjamin.dk>
2019-02-26 12:04:08 +0100

    REV check image data-src

--- lib/rails_amp/view_helpers/image_tag_helper.rb
+++ lib/rails_amp/view_helpers/image_tag_helper.rb
@@ -15,7 +15,7 @@ module RailsAmp
         options = options.symbolize_keys
         check_for_image_tag_errors(options) if defined?(check_for_image_tag_errors)
 
-        src = options[:src] = options[:data].try([:src]) || path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+        src = options[:src] = options[:data].try(:[], :src) || path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
 
         unless src.start_with?("cid:") || src.start_with?("data:") || src.blank?
           options[:alt] = options.fetch(:alt){ image_alt(src) }
